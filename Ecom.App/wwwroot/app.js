56787d06b342d909352f66abd8dc4005984cf2a9
Md. Monir Hossain <moniraiub123@gmail.com>
2020-08-12 01:42:40 -0700

    Dashboard added successfully with partial view

---
+++ Ecom.App/wwwroot/app.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="angular/angular.js" />
+
+
+'use strict';
+var app = angular.module("app", []);
+
+app.controller("myTestController", function myTestController($scope) {
+    $scope.message = "Successfully added angularjs";
+});
